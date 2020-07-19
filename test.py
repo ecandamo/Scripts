edc460149c4951ad896bfce0765ac27c8b9411e2
ecandamo <esteban.candamo@gmail.com>
2020-07-19 01:39:58 -0400

    deleted test script

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.DataFrame(np.random.randint(10, 100, size=(2, 5), dtype=int))
-#print(df)
-
-df2 = pd.DataFrame(np.random.rand(2, 5) * 100)
-print(df2.head())
